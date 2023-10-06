095fdd3129f3bf751dae015537b99776dc94df3e
Arthur <costa.arthur.lima@gmail.com>
2023-10-06 01:05:55 -0300

    refatoração

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,9 +8,8 @@ using UnityEngine.Rendering.Universal;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector2 moveDir;
+    public float HorizontalDir { get; private set;}
     private Vector2 velocity;
-    private Vector2 oldVelocity; // pra guardar a velocidade antes do dash
 
     
     [SerializeField] private float speed = 10f;
@@ -24,7 +23,7 @@ public class PlayerMovement : MonoBehaviour
     private bool isDashing = false;
     private float timeSinceDash = 0;
     private Vector2 dashDir;
-    private float dashSpeed => dashDistance / dashDuration;
+    private float DashSpeed => dashDistance / dashDuration;
 
 
     [Header("Checks")]
@@ -32,63 +31,34 @@ public class PlayerMovement : MonoBehaviour
     private bool collisionUp, collisionDown;
 
     [Header("Ground Check")]
-    [SerializeField] private Transform groundCheckTransform;
+    [SerializeField] private Vector3 lowerCollisionBound;
     [SerializeField] private float groundDitstance;
-    private bool isOnFLoor => collisionDown && velocity.y <= 0; // esta no chao se colidindo com o chao e nao esta subindo
+    public bool IsOnFLoor => collisionDown && velocity.y <= 0; // esta no chao se colidindo com o chao e nao esta subindo
     private bool wasOnAir; // estava no ar no ultimo frame
-    private bool isLanding => isOnFLoor && wasOnAir; // estava no ar e tocou o chao NESTE frame
+    public bool isLanding => IsOnFLoor && wasOnAir; // estava no ar e tocou o chao NESTE frame
 
     [Header("Cealing Check")]
-    [SerializeField] private Transform cealingCheckTransform;
+    [SerializeField] private Vector3 upperCollisionBound;
     [SerializeField] private float cealingDitstance;
-    private bool isOnCealing => collisionUp && velocity.y >= 0;
+    public bool IsOnCealing => collisionUp && velocity.y >= 0;
 
     private float gravity;
     private bool isGravityEnabled = true;
+    private Collider2D coll;
+
+
+    private void Start() {
+        coll = GetComponent<Collider2D>();
+    }
 
     private void Update() {
-        moveDir = Vector2.zero;
-        moveDir.x = Input.GetAxisRaw("Horizontal");
+        HorizontalDir = 0;
+        HorizontalDir = Input.GetAxisRaw("Horizontal");
 
         // Pulo
         if (Input.GetKeyDown(KeyCode.Space)) {
             velocity.y = jumpForce;
             gravity = 0;
-        }
-
-        // Espada
-        if (Input.GetKeyDown(KeyCode.Mouse0) && !isDashing) {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            dashDir = (mousePos - (Vector2)transform.position);
-            if (isOnFLoor) {
-                dashDir.y = Mathf.Clamp(dashDir.y, 0, 1);
-            }
-            dashDir.Normalize();
-
-            isDashing = true;
-            timeSinceDash = 0;
-            oldVelocity = velocity;
-
-            Vector3 from = transform.position;
-            Vector3 to = from + (Vector3)dashDir * dashDistance;
-            Debug.DrawLine(from, to, Color.green, dashDuration *2 );
-        }
-
-        // Arma
-        if (Input.GetKeyDown(KeyCode.Mouse1) && !isDashing) {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            dashDir = -(mousePos - (Vector2)transform.position);
-            dashDir += moveDir; // mistura a direção do mouse com a do teclado
-            dashDir.Normalize();
-
-            if (!isOnFLoor || dashDir.y > 0) { // se estiver no chao e atirar pra cima, não executa o dash (talvez aumentar um pouco o 0 seja interessante)
-                isDashing = true;
-                timeSinceDash = 0;
-
-                Vector3 from = transform.position;
-                Vector3 to = from - (Vector3)dashDir * dashDistance;
-                Debug.DrawLine(from, to, Color.yellow, dashDuration *2 );
-            }
         }
     }
 
@@ -97,19 +67,19 @@ public class PlayerMovement : MonoBehaviour
         HandleColisions();
 
         if (isDashing)
-            Dash();
+            UpdateDash();
         else {    
-            if (isOnCealing) {
+            if (IsOnCealing) {
                 velocity.y = -velocity.y;
             }
 
-            if (isOnFLoor) {
+            if (IsOnFLoor) {
                 gravity = 0f;
                 velocity.y = 0f;
-                velocity.x = moveDir.x * speed;
+                velocity.x = HorizontalDir * speed;
             }
             else {
-                velocity.x += moveDir.x * speed * airControl;
+                velocity.x += HorizontalDir * speed * airControl;
                 velocity.x = Mathf.Clamp(velocity.x, -speed, speed);
                 if (isGravityEnabled) {
                     gravity += Gravity * Time.fixedDeltaTime;
@@ -122,10 +92,28 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
-    private void Dash() {
+    /// <summary>
+    /// Executa o dash caso outro dash não estaja em execução
+    /// </summary>
+    /// <param name="direction">direção do dash (precisar ser normalizado)</param>
+    /// <param name="distance">distancia percorrida durante o dash</param>
+    /// <param name="duration">tempo para percorrer a distancia do dash</param>
+    public void Dash(Vector2 direction, float distance, float duration) {
+        if (isDashing)
+            return;
+        
+        dashDir = direction;
+        dashDistance = distance;
+        dashDuration = duration;
+
+        isDashing = true;
+        timeSinceDash = 0;
+    }
+
+    private void UpdateDash() { // chamar no fixedUpdate
         timeSinceDash += Time.fixedDeltaTime;
-        Debug.Log(isOnFLoor);
-        velocity = dashDir * dashSpeed;
+        Debug.Log(IsOnFLoor);
+        velocity = dashDir * DashSpeed;
         if(timeSinceDash >= dashDuration || isLanding) {
             isDashing = false;
             isGravityEnabled = true;
@@ -137,10 +125,16 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void HandleColisions() {
-        wasOnAir = !isOnFLoor;
+        lowerCollisionBound.x = coll.bounds.center.x;
+        lowerCollisionBound.y = coll.bounds.center.y - coll.bounds.extents.y;
+        upperCollisionBound.x = coll.bounds.center.x;
+        upperCollisionBound.y = coll.bounds.center.y + coll.bounds.extents.y;
 
-        collisionUp = Physics2D.Raycast(cealingCheckTransform.position, Vector2.down, cealingDitstance, groundLayer);
-        collisionDown = Physics2D.Raycast(groundCheckTransform.position, Vector2.down, groundDitstance, groundLayer);
+
+        wasOnAir = !IsOnFLoor;
+
+        collisionUp = Physics2D.Raycast(upperCollisionBound, Vector2.down, cealingDitstance, groundLayer);
+        collisionDown = Physics2D.Raycast(lowerCollisionBound, Vector2.down, groundDitstance, groundLayer);
     }
 
 
@@ -148,11 +142,11 @@ public class PlayerMovement : MonoBehaviour
     {
         Gizmos.color = Color.red;
 
-        Vector3 from = groundCheckTransform.position;
+        Vector3 from = lowerCollisionBound;
         Vector3 to = from + Vector3.down * groundDitstance;
         Gizmos.DrawLine(from, to);
 
-        from = cealingCheckTransform.position;
+        from = upperCollisionBound;
         to = from + Vector3.up * groundDitstance;
         Gizmos.DrawLine(from, to);
     }
