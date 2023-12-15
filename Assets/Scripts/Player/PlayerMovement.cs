fc5f05fcbb22b26b38643767ddaa05ea626ac6ff
Arthur <costa.arthur.lima@gmail.com>
2023-12-14 21:33:09 -0300

    delete SampleScene

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
-using Unity.VisualScripting;
-using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
